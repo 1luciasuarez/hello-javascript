40df41b2d8c6fc9043c6375369c7a0d5bf991b54
Luci <luciasuarez172@gmail.com>
2025-06-14 15:58:33 -0300

    mi avance hasta ahora

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -1,36 +1,18 @@
-/*
-Clases 1 a 14 (sin código)
-Vídeo: https://youtu.be/1glVfFxj8a4
+console.log('¡Hola, Javascript!');
+console.log('¡Hola, Javascript!');
+console.log(`¡Hola, Javascript!`);
 
-Clase 15 - Hola mundo
-Vídeo: https://youtu.be/1glVfFxj8a4?t=2390
-*/
-
-// Esto es un comentario simple
+//comentario simple
 
 /*
-Esto es
-un comentario
-en varias líneas
+comentario
 */
 
-console.log("¡Hola, JavaScript!")
-console.log('¡Hola, JavaScript!')
-console.log(`¡Hola, JavaScript!`)
-
-// console.log("¡Hola, JavaScript!")
-
-/*
-console.log("¡Hola, JavaScript!")
-console.log('¡Hola, JavaScript!')
-console.log(`¡Hola, JavaScript!`)
-*/
-
-console.log("5")
-console.log(5)
-console.log(5 + 2)
-console.log(5 - 2)
-console.log(5 * 2)
-console.log(5 / 2)
-console.log(5 % 2)
-console.log(5 ** 2)
+console.log('5');
+console.log(5);
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2);
--- Basic/01-variables.js
+++ Basic/01-variables.js
@@ -1,29 +1,24 @@
-/*
-Clase 16 - Variables
-Vídeo: https://youtu.be/1glVfFxj8a4?t=3049
-*/
+//variables; var, let, const
 
-// var
+//var (no recomendada)
 
-var helloWorld = "¡Hola, JavaScript!"
-console.log(helloWorld)
+var helloWorld = '¡Hola, Javascript!';
+console.log(helloWorld);
 
-helloWorld = "¡Hola de nuevo, JavaScript!"
-console.log(helloWorld)
+helloWorld = 'Hola de nuevo, JS';
+console.log(helloWorld);
 
-// let
+//let
 
-let helloWorld2 = "¡Hola, JavaScript 2!"
-console.log(helloWorld2)
+let helloWorld2 = '¡Hola, Javascript! 2';
+console.log(helloWorld2);
 
-helloWorld2 = "¡Hola de nuevo, JavaScript 2!"
-console.log(helloWorld2)
+helloWorld2 = 'Hola de nuevo, JS 2';
+console.log(helloWorld2);
 
-// const
-
-const helloWorld3 = "¡Hola, JavaScript 3!"
-console.log(helloWorld3)
-
-// Error
-// helloWorld3 = "¡Hola de nuevo, JavaScript 2!"
-// console.log(helloWorld3)
+//const
+const helloWorld3 = '¡Hola, Javascript! 3 ';
+console.log(helloWorld3);
+//error
+// const helloWorld3 = 'Hola de nuevo, JS 3';
+//console.log(helloWorld3);
